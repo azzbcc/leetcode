e7feb6000b74fb94b64199062eb64702c0818120
Clarence <xjh.azzbcc@gmail.com>
2020-08-05 12:26:16 +0800

    0011 盛最多水的容器 解决

---
+++ base/0011/suites.c
@@ -0,0 +1,26 @@
+/**
+ * suites.c
+ * Copyright(c) 2020 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2020/8/5 上午12:10
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <check.h>
+#include <stdlib.h>
+#include <check_stdint.h>
+#include <stdbool.h>
+
+#include "0011.c"
+
+START_TEST(test_official) {
+    int arr[]  = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+    int target = 49, ans = maxArea(arr, sizeof(arr) / sizeof(arr[0]));
+
+    ck_assert_msg(target == ans, "error, except %d but got %d on rob().", target, ans);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_official);
+}
