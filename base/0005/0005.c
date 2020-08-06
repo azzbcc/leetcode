da407e7162e1199d8e0c9522bf5d04b6c5791f65
Clarence <xjh.azzbcc@gmail.com>
2020-08-06 15:17:55 +0800

    0005 最长回文字串 优化

--- base/0005/0005.c
+++ base/0005/0005.c
@@ -15,7 +15,7 @@
 // Related Topics 字符串 动态规划
 // 👍 2502 👎 0
 #define MAX 1000
-char *longestPalindrome(char *str) {
+char *longestPalindrome_1(char *str) {
     int max   = 0;
     char *tmp = str, *ans = NULL;
     bool data[MAX][MAX] = { false };
@@ -38,3 +38,48 @@ char *longestPalindrome(char *str) {
 
     return ans;
 }
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+char *expand(char *s, int *len) {
+    static char str[MAX * 2 + 2] = { '#' };
+
+    *len = 1;
+    while (*s) {
+        str[(*len)++] = *s++;
+        str[(*len)++] = '#';
+    }
+    str[*len] = '\0';
+    return str;
+}
+char *longestPalindrome_2(char *str) {
+    int len = 0, max = 0, B[MAX * 2 + 1] = { 0 };
+    char *tmp = str, *S = expand(str, &len);
+
+    for (int P = 0, C = 0, R = 0; P < len; ++P) {
+        if (R > P) {
+            int P_ = 2 * C - P;
+            B[P]   = min(B[P_], R - P);
+            if (R - P != B[P_]) continue;
+        } else {
+            R = P;
+        }
+        C = P;
+
+        int R_ = 2 * C - R;
+        while (R_ > 0 && R + 1 < len && S[R_ - 1] == S[R + 1]) {
+            B[P]++, R_--, R++;
+        }
+
+        if (max < B[P]) max = B[P], tmp = str + (P - B[P]) / 2;
+    }
+
+    char *ans = calloc(max + 1, sizeof(char));
+    memcpy(ans, tmp, max);
+    ans[max] = '\0';
+
+    return ans;
+}
+
+char *(*longestPalindrome)(char *) = longestPalindrome_2;
