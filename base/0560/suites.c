a7c079ab785e37dd188f4793a3db7454b29edd4f
Clarence <xjh.azzbcc@gmail.com>
2020-08-27 17:51:05 +0800

    0560 和为K的子数组 解决

---
+++ base/0560/suites.c
@@ -0,0 +1,22 @@
+/**
+ * suites.c
+ * Copyright(c) 2020 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2020/8/27 下午5:31
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+
+#include "0560.c"
+
+START_TEST(test_official) {
+    int arr[] = { 1, 1, 1 }, k = 2;
+    int target = 2, ans = subarraySum(arr, sizeof(arr) / sizeof(arr[0]), k);
+    ck_assert_int_eq(target, ans);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_official);
+}
