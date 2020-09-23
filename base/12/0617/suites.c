e4d49bcb8078db18f52a58113f7c6f2a165d40d8
Clarence <xjh.azzbcc@gmail.com>
2020-09-23 11:37:05 +0800

    0617 合并二叉树 解决

---
+++ base/12/0617/suites.c
@@ -0,0 +1,34 @@
+/**
+ * suites.c
+ * Copyright(c) 2020 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2020/9/23 上午11:12
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+#include <tree.h>
+
+#include "0617.c"
+
+START_TEST(test_official) {
+    int arr1[] = { 1, 3, 2, 5 }, arr2[] = { 2, 1, 3, null, 4, null, 7 };
+    int target[] = { 3, 4, 5, 5, 4, null, 7 };
+
+    tree_t t1 = tree_create(arr1, sizeof(arr1) / sizeof(arr1[0]));
+    tree_t t2 = tree_create(arr2, sizeof(arr2) / sizeof(arr2[0]));
+    tree_t tt = tree_create(target, sizeof(target) / sizeof(target[0]));
+
+    tree_t ans = mergeTrees(t1, t2);
+    ck_assert(tree_equal(tt, ans));
+
+    tree_free(t1);
+    tree_free(t2);
+    tree_free(tt);
+    tree_free(ans);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_official);
+}
