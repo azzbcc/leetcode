1c7612217bcbfa265abccedb2c83554dc3678398
Clarence <xjh.azzbcc@gmail.com>
2020-09-30 11:18:00 +0800

    0701 二叉搜索树中的插入操作 解决

---
+++ base/14/0701/suites.c
@@ -0,0 +1,30 @@
+/**
+ * suites.c
+ * Copyright(c) 2020 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2020/9/30 上午11:06
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+#include <tree.h>
+
+#include "0701.c"
+
+START_TEST(test_official) {
+    int arr[] = { 4, 2, 7, 1, 3 }, target[] = { 4, 2, 7, 1, 3, 5 };
+    tree_t ta = tree_create(arr, sizeof(arr) / sizeof(arr[0]));
+    tree_t tt = tree_create(target, sizeof(target) / sizeof(target[0]));
+
+    tree_t ans = insertIntoBST(ta, 5);
+
+    ck_assert(tree_equal(ans, tt));
+
+    tree_free(tt);
+    tree_free(ans);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_official);
+}
