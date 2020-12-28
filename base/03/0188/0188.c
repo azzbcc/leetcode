175b5f66cc6932fde77fa95958d2fc7436bac319
Clarence <xjh.azzbcc@gmail.com>
2020-12-28 19:03:14 +0800

    0188 买卖股票的最佳时机IV 优秀解法摘录

--- base/03/0188/0188.c
+++ base/03/0188/0188.c
@@ -88,7 +88,7 @@ int maxProfit(int K, int *prices, int size) {
 
     return dp[last][len - 1];
 }
-#else
+#elif 0
 int max(int a, int b) {
     return a > b ? a : b;
 }
@@ -109,5 +109,63 @@ int maxProfit(int K, int *prices, int size) {
     }
 
     return dp[K][0];
+}
+#else
+// [一种基于wqs二分的做法](https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv/solution/yi-chong-ji-yu-wqs-er-fen-de-you-xiu-zuo-x36r)
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+int maxProfit(int K, int *prices, int size) {
+    if (size <= 0) return 0;
+
+    // 二分查找的上下界
+    int left = 1, right = 0;
+    for (int i = 0; i < size; ++i) {
+        if (right < prices[i]) right = prices[i];
+    }
+
+    // 存储答案，如果值为 -1 表示二分查找失败
+    int ans = -1;
+    while (left <= right) {
+        // 二分得到当前的斜率（手续费）
+        int c = (left + right) / 2;
+
+        // 使用与 714 题相同的动态规划方法求解出最大收益以及对应的交易次数
+        int buyCount = 0, sellCount = 0;
+        int buy = -prices[0], sell = 0;
+
+        for (int i = 1; i < size; ++i) {
+            if (sell - prices[i] >= buy) {
+                buy      = sell - prices[i];
+                buyCount = sellCount;
+            }
+            if (buy + prices[i] - c >= sell) {
+                sell      = buy + prices[i] - c;
+                sellCount = buyCount + 1;
+            }
+        }
+
+        // 如果交易次数大于等于 k，那么可以更新答案
+        // 这里即使交易次数严格大于 k，更新答案也没有关系，因为总能二分到等于 k 的
+        if (sellCount >= K) {
+            // 别忘了加上 kc
+            ans  = sell + K * c;
+            left = c + 1;
+        } else {
+            right = c - 1;
+        }
+    }
+
+    // 如果二分查找失败，说明交易次数的限制不是瓶颈
+    // 可以看作交易次数无限，直接使用贪心方法得到答案
+    if (ans == -1) {
+        ans = 0;
+        for (int i = 1; i < size; ++i) {
+            ans += max(prices[i] - prices[i - 1], 0);
+        }
+    }
+
+    return ans;
 }
 #endif
