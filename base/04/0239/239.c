b4ce75f8ae140ab0f9eaa70e2fa9526ae75e98c9
Clarence <xjh.azzbcc@gmail.com>
2021-01-02 14:35:44 +0800

    0239 滑动窗口最大值 解决

---
+++ base/04/0239/239.c
@@ -0,0 +1,106 @@
+// 给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k
+// 个数字。滑动窗口每次只向右移动一位。
+//
+// 返回滑动窗口中的最大值。
+//
+//
+//
+// 示例 1：
+//
+//
+// 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+// 输出：[3,3,5,5,6,7]
+// 解释：
+// 滑动窗口的位置                最大值
+// ---------------               -----
+// [1  3  -1] -3  5  3  6  7       3
+//  1 [3  -1  -3] 5  3  6  7       3
+//  1  3 [-1  -3  5] 3  6  7       5
+//  1  3  -1 [-3  5  3] 6  7       5
+//  1  3  -1  -3 [5  3  6] 7       6
+//  1  3  -1  -3  5 [3  6  7]      7
+//
+//
+// 示例 2：
+//
+//
+// 输入：nums = [1], k = 1
+// 输出：[1]
+//
+//
+// 示例 3：
+//
+//
+// 输入：nums = [1,-1], k = 1
+// 输出：[1,-1]
+//
+//
+// 示例 4：
+//
+//
+// 输入：nums = [9,11], k = 2
+// 输出：[11]
+//
+//
+// 示例 5：
+//
+//
+// 输入：nums = [4,-2], k = 2
+// 输出：[4]
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+// 1 <= k <= nums.length
+//
+// Related Topics 堆 Sliding Window
+// 👍 725 👎 0
+
+typedef struct {
+    int size, front, rear;
+    int data[];
+} queue_t;
+void queue_init(queue_t *q, int size) {
+    q->size = size, q->front = q->rear = 0;
+}
+bool queue_empty(queue_t *q) {
+    return q->front == q->rear;
+}
+int queue_right(queue_t *q) {
+    return q->data[(q->rear + q->size - 1) % q->size];
+}
+void queue_rpop(queue_t *q) {
+    q->rear = (q->rear + q->size - 1) % q->size;
+}
+void queue_rpush(queue_t *q, int val) {
+    while (!queue_empty(q) && queue_right(q) < val) {
+        queue_rpop(q);
+    }
+    q->data[q->rear] = val, q->rear = (q->rear + 1) % q->size;
+}
+int queue_left(queue_t *q) {
+    return q->data[q->front];
+}
+void queue_lpop(queue_t *q) {
+    q->front = (q->front + 1) % q->size;
+}
+int *maxSlidingWindow(int *nums, int size, int k, int *returnSize) {
+    int memory[sizeof(queue_t) / sizeof(int) + k + 1];
+    queue_t *q = ( queue_t * )memory;
+    int *ans   = malloc((*returnSize = size - k + 1) * sizeof(int));
+
+    queue_init(q, k + 1);
+    for (int i = 0; i < k; queue_rpush(q, nums[i++])) {}
+    for (int i = 0; i + k < size; ++i) {
+        ans[i] = queue_left(q);
+        if (nums[i] == ans[i]) queue_lpop(q);
+        queue_rpush(q, nums[i + k]);
+    }
+    ans[size - k] = queue_left(q);
+
+    return ans;
+}
---
+++ base/04/0239/suites.c
@@ -0,0 +1,92 @@
+/**
+ * suites.c
+ * Copyright(c) 2021 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2021/1/2 下午1:21
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+
+#include "239.c"
+
+int queue_length(queue_t *q) {
+    return (q->rear + q->size - q->front) % q->size;
+}
+
+START_TEST(test_queue) {
+    int arr[] = { 1, 1, 1, 1, 1 };
+    int memory[sizeof(queue_t) / sizeof(int) + LEN(arr) + 1];
+    queue_t *q = ( queue_t * )memory;
+
+    queue_init(q, LEN(arr) + 1);
+    for (int i = 0; i < LEN(arr); ++i) {
+        queue_rpush(q, arr[i]);
+        ck_assert_int_eq(queue_length(q), i + 1);
+    }
+}
+
+START_TEST(test_official_1) {
+    int nums[] = { 1, 3, -1, -3, 5, 3, 6, 7 }, k = 3;
+    int target[] = { 3, 3, 5, 5, 6, 7 };
+    int len, *ans = maxSlidingWindow(nums, LEN(nums), k, &len);
+
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
+
+    free(ans);
+}
+
+START_TEST(test_official_2) {
+    int nums[] = { 1 }, k = 1;
+    int target[] = { 1 };
+    int len, *ans = maxSlidingWindow(nums, LEN(nums), k, &len);
+
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
+
+    free(ans);
+}
+
+START_TEST(test_official_3) {
+    int nums[] = { 1, -1 }, k = 1;
+    int target[] = { 1, -1 };
+    int len, *ans = maxSlidingWindow(nums, LEN(nums), k, &len);
+
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
+
+    free(ans);
+}
+
+START_TEST(test_official_4) {
+    int nums[] = { 9, 11 }, k = 2;
+    int target[] = { 11 };
+    int len, *ans = maxSlidingWindow(nums, LEN(nums), k, &len);
+
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
+
+    free(ans);
+}
+
+START_TEST(test_official_5) {
+    int nums[] = { 4, -2 }, k = 2;
+    int target[] = { 4 };
+    int len, *ans = maxSlidingWindow(nums, LEN(nums), k, &len);
+
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
+
+    free(ans);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_queue);
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+    tcase_add_test(t, test_official_3);
+    tcase_add_test(t, test_official_4);
+    tcase_add_test(t, test_official_5);
+}
