458d2af947355f0cc37c40f2eb868fe45ca72a82
Clarence <xjh.azzbcc@gmail.com>
2021-02-19 01:56:29 +0800

    0001 两数之和 重做

--- base/00/0001/0001.c
+++ base/00/0001/0001.c
@@ -1,33 +1,67 @@
-//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
 //
 // 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
 //
+// 你可以按任意顺序返回答案。
 //
 //
-// 示例:
 //
-// 给定 nums = [2, 7, 11, 15], target = 9
+// 示例 1：
 //
-//因为 nums[0] + nums[1] = 2 + 7 = 9
-//所以返回 [0, 1]
+//
+// 输入：nums = [2,7,11,15], target = 9
+// 输出：[0,1]
+// 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+//
+//
+// 示例 2：
+//
+//
+// 输入：nums = [3,2,4], target = 6
+// 输出：[1,2]
+//
+//
+// 示例 3：
+//
+//
+// 输入：nums = [3,3], target = 6
+// 输出：[0,1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 2 <= nums.length <= 103
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// 只会存在一个有效答案
 //
 // Related Topics 数组 哈希表
-// 👍 8714 👎 0
+// 👍 10299 👎 0
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int* ans = calloc(*returnSize = 2, sizeof(int));
+typedef struct {
+    int key, index;
+    UT_hash_handle hh;
+} hash_t;
+int *twoSum(int *nums, int size, int target, int *returnSize) {
+    hash_t *hash = NULL, *cur = NULL, *next = NULL;
+    int *ans = malloc((*returnSize = 2) * sizeof(int));
 
-    for (int i = 0; i < numsSize; ++i) {
-        int left = target - nums[i];
-        for (int j = i + 1; j < numsSize; ++j) {
-            if (left == nums[j]) {
-                ans[0] = i, ans[1] = j;
-                return ans;
-            }
+    for (int i = 0; i < size; ++i) {
+        HASH_FIND_INT(hash, &nums[i], cur);
+        if (cur) {
+            ans[0] = cur->index, ans[1] = i;
+            break;
         }
+        cur      = malloc(sizeof(hash_t));
+        cur->key = target - nums[i], cur->index = i;
+        HASH_ADD_INT(hash, key, cur);
     }
-    return NULL;
+    HASH_ITER(hh, hash, cur, next) {
+        HASH_DEL(hash, cur);
+        free(cur);
+    }
+    return ans;
 }
--- base/00/0001/suites.c
+++ base/00/0001/suites.c
@@ -7,21 +7,39 @@
  * Modified   :
  *     Author : Clarence <xjh.azzbcc@gmail.com>
  */
-#include <check.h>
-#include <stdlib.h>
+#include <common.h>
 
 #include "0001.c"
 
-START_TEST(test_official) {
-    int arr[] = { 2, 7, 11, 15 }, target = 9, retSize;
-    int *ans = twoSum(arr, sizeof(arr) / sizeof(arr[0]), target, &retSize);
+START_TEST(test_official_1) {
+    int arr[] = { 2, 7, 11, 15 }, t = 9, target[] = { 0, 1 };
+    int len, *ans = twoSum(arr, LEN(arr), t, &len);
 
-    ck_assert_int_eq(retSize, 2);
-    ck_assert_msg(ans[0] == 0 && ans[1] == 1, "error, except [0, 1] but got [%d, %d] on twoSum([2, 7, 11, 15], 9).", ans[0], ans[1]);
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
+    free(ans);
+}
 
+START_TEST(test_official_2) {
+    int arr[] = { 3, 2, 4 }, t = 6, target[] = { 1, 2 };
+    int len, *ans = twoSum(arr, LEN(arr), t, &len);
+
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
+    free(ans);
+}
+
+START_TEST(test_official_3) {
+    int arr[] = { 3, 3 }, t = 6, target[] = { 0, 1 };
+    int len, *ans = twoSum(arr, LEN(arr), t, &len);
+
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
     free(ans);
 }
 
 void tcase_complete(TCase *t) {
-    tcase_add_test(t, test_official);
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+    tcase_add_test(t, test_official_3);
 }
