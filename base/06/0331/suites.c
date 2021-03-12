7511a688c2093671545660e5463cd191318dc37d
Clarence <xjh.azzbcc@gmail.com>
2021-03-12 12:31:11 +0800

    0331 验证二叉树的前序序列化 解决

---
+++ base/06/0331/suites.c
@@ -0,0 +1,51 @@
+/**
+ * suites.c
+ * Copyright(c) 2021 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2021/3/12 下午12:11
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+
+#include "0331.c"
+
+START_TEST(test_official_1) {
+    char *s = "9,3,4,#,#,1,#,#,2,#,6,#,#";
+    ck_assert_int_eq(true, isValidSerialization(s));
+}
+
+START_TEST(test_official_2) {
+    char *s = "1,#";
+    ck_assert_int_eq(false, isValidSerialization(s));
+}
+
+START_TEST(test_official_3) {
+    char *s = "9,#,#,1";
+    ck_assert_int_eq(false, isValidSerialization(s));
+}
+
+START_TEST(test_failed_1) {
+    char *s = "9,#,92,#,#";
+    ck_assert_int_eq(true, isValidSerialization(s));
+}
+
+START_TEST(test_failed_2) {
+    char *s = "#";
+    ck_assert_int_eq(true, isValidSerialization(s));
+}
+
+START_TEST(test_failed_3) {
+    char *s = "#,7,6,9,#,#,#";
+    ck_assert_int_eq(false, isValidSerialization(s));
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_failed_1);
+    tcase_add_test(t, test_failed_2);
+    tcase_add_test(t, test_failed_3);
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+    tcase_add_test(t, test_official_3);
+}
