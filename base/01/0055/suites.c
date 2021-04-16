19e992f510e854c62e2ac1a619f5de86ea21f9e4
Clarence <xjh.azzbcc@gmail.com>
2021-04-16 16:35:39 +0800

    0055 跳跃游戏 解决

---
+++ base/01/0055/suites.c
@@ -0,0 +1,36 @@
+/**
+ * suites.c
+ * Copyright(c) 2021 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2021/4/16 下午3:29
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+
+#include "0055.c"
+
+START_TEST(test_official_1) {
+    int arr[]   = { 2, 3, 1, 1, 4 };
+    bool target = true, ans = canJump(arr, LEN(arr));
+    ck_assert_int_eq(ans, target);
+}
+
+START_TEST(test_official_2) {
+    int arr[]   = { 3, 2, 1, 0, 4 };
+    bool target = false, ans = canJump(arr, LEN(arr));
+    ck_assert_int_eq(ans, target);
+}
+
+START_TEST(test_own) {
+    int arr[]   = { 3, 2, 1, 0 };
+    bool target = true, ans = canJump(arr, LEN(arr));
+    ck_assert_int_eq(ans, target);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_own);
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+}
