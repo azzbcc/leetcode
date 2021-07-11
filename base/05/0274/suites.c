78af091b66b9f2d880b8fb5ae3dc28c8060d1de7
Clarence <xjh.azzbcc@gmail.com>
2021-07-11 13:07:37 +0800

    0274 H指数 解决

---
+++ base/05/0274/suites.c
@@ -0,0 +1,36 @@
+/**
+ * suites.c
+ * Copyright(c) 2021 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2021/7/11 下午12:54
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+
+#include "0274.c"
+
+START_TEST(test_official) {
+    int arr[]  = { 3, 0, 6, 1, 5 };
+    int target = 3, ans = hIndex(arr, LEN(arr));
+    ck_assert_int_eq(ans, target);
+}
+
+START_TEST(test_failed_1) {
+    int arr[]  = { 100 };
+    int target = 1, ans = hIndex(arr, LEN(arr));
+    ck_assert_int_eq(ans, target);
+}
+
+START_TEST(test_failed_2) {
+    int arr[]  = { 4, 4, 0, 0 };
+    int target = 2, ans = hIndex(arr, LEN(arr));
+    ck_assert_int_eq(ans, target);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_failed_1);
+    tcase_add_test(t, test_failed_2);
+    tcase_add_test(t, test_official);
+}
