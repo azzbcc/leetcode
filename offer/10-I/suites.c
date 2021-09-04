02da3dbaf133a391d48418e9c3f8e3d5fd46b883
Clarence <xjh.azzbcc@gmail.com>
2021-09-04 21:08:28 +0800

    剑指Offer10-I 斐波那契数列 解决

---
+++ offer/10-I/suites.c
@@ -0,0 +1,28 @@
+/**
+ * suites.c
+ * Copyright(c) 2021 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2021/9/4 下午8:57
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+
+#include "offer_10-I.c"
+
+START_TEST(test_official_1) {
+    int n      = 2;
+    int target = 1, ans = fib(n);
+    ck_assert_int_eq(ans, target);
+}
+START_TEST(test_official_2) {
+    int n      = 5;
+    int target = 5, ans = fib(n);
+    ck_assert_int_eq(ans, target);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+}
