53389ed39f0aaab7a2a57f446ecbeb3290528c70
Clarence <xjh.azzbcc@gmail.com>
2021-09-22 19:20:03 +0800

    1738 找出第K大的异或坐标值 解决栈溢出问题

--- base/17/1738/1738.c
+++ base/17/1738/1738.c
@@ -45,25 +45,27 @@
 // Related Topics 数组
 // 👍 44 👎 0
 
-int map[1000 * 1000];
 int cmp(const void *a, const void *b) {
-    return *(int *)b - *(int *)a;
+    return *( int * )b - *( int * )a;
 }
-int kthLargestValue(int** matrix, int matrixSize, int* matrixColSize, int k){
-    int m = matrixSize, n = *matrixColSize;
+int kthLargestValue(int **matrix, int matrixSize, int *matrixColSize, int k) {
+    int m = matrixSize, n = *matrixColSize, *map = malloc(m * n * sizeof(int));
 
     map[0] = matrix[0][0];
-    for (int i = 1;i < n;i++) {
+    for (int i = 1; i < n; i++) {
         map[i] = matrix[0][i] = matrix[0][i - 1] ^ matrix[0][i];
     }
-    for (int i = 1;i < m;i++) {
+    for (int i = 1; i < m; i++) {
         map[i * n] = matrix[i][0] = matrix[i - 1][0] ^ matrix[i][0];
-        for (int j = 1;j < n;j++) {
+        for (int j = 1; j < n; j++) {
             map[i * n + j] = matrix[i][j] = matrix[i][j - 1] ^ matrix[i - 1][j] ^ matrix[i - 1][j - 1] ^ matrix[i][j];
         }
     }
 
     qsort(map, m * n, sizeof(int), cmp);
 
-    return map[k - 1];
+    int ans = map[k - 1];
+    free(map);
+
+    return ans;
 }
--- base/17/1738/suites.c
+++ base/17/1738/suites.c
@@ -40,10 +40,31 @@ START_TEST(test_official_4) {
 }
 
 START_TEST(test_tle) {
-    int col = 1000, k = 114257;
-#include "tle.c"
-    int target = 14, ans = kthLargestValue(matrix, LEN(matrix), &col, k);
+    char current[] = __FILE__;
+
+    sprintf(strrchr(current, '/'), "/tle.txt");
+    freopen(current, "rb", stdin);
+
+    int k = 114257, len = 0, cur = 0, deep = 0;
+    int *matrix[0x400] = { NULL }, col[0x400] = { 0 };
+    for (char ch; EOF != scanf("%c", &ch);) {
+        if (ch == '[') {
+            if (++deep == 2) matrix[len] = malloc(0x400 * sizeof(int));
+        } else if (isdigit(ch)) {
+            cur *= 10, cur += ch - '0';
+        } else if (deep == 2 && (ch == ',' || ch == ']')) {
+            matrix[len][col[len]++] = cur, cur = 0;
+            if (ch == ']') len++, deep--;
+        }
+    }
+    fclose(stdin);
+
+    int target = 14, ans = kthLargestValue(matrix, len, col, k);
     ck_assert_int_eq(ans, target);
+
+    for (int i = 0; i < len; ++i) {
+        free(matrix[i]);
+    }
 }
 
 void tcase_complete(TCase *t) {
