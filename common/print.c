0eb427cd3e085d75f999ef8f4edd5e3cc14aa80e
Clarence <xjh.azzbcc@gmail.com>
2021-10-12 18:35:22 +0800

    common 使用二级指针表示动态树

--- common/print.c
+++ common/print.c
@@ -17,7 +17,7 @@
  * @param flags            标记当前节点是左孩子还是右孩子
  * @param print_tree_node  打印回调函数（函数中不可换行）
  */
-static void print_static_tree_real(void *tree, int *offsets, int size, int depth, bool flags[],
+static void print_static_tree_real(void *tree, int offsets[], int size, int depth, bool flags[],
                                    void (*print_tree_node)(void *)) {
     for (int i = 0; i < depth - 1; i++) {
         printf(" %s  ", flags[i] ? "│" : " ");
@@ -35,13 +35,13 @@ static void print_static_tree_real(void *tree, int *offsets, int size, int depth
     bool has_child = false;
     for (int i = 0; !has_child && i < size; ++i) {
         void *next = (*( void ** )tree) + offsets[i];
-        has_child  = *( void ** )next;
+        has_child  = *( void  **)next;
     }
     if (!has_child) return;
 #endif
 
     for (int i = 0; i < size; ++i) {
-        void *next   = (*( void ** )tree) + offsets[i];
+        void *next   = (*( void   **)tree) + offsets[i];
         flags[depth] = i + 1 < size;
         print_static_tree_real(next, offsets, size, depth + 1, flags, print_tree_node);
     }
@@ -82,21 +82,21 @@ static void print_dynamic_tree_real(void *tree, int count_offset, int point_offs
     print_tree_node(*( void ** )tree);
     printf("\n");
 
-    int size = *(int *)((*( void ** )tree) + count_offset);
+    int size = *( int * )((*( void ** )tree) + count_offset);
 
 #if 1
     bool has_child = false;
     for (int i = 0; !has_child && i < size; ++i) {
-        void *next = (*( void ** )tree) + point_offset + i * sizeof(void *);
-        has_child  = *( void  **)next;
+        void ***next = (*( void ** )tree) + point_offset;
+        has_child    = **next;
     }
     if (!has_child) return;
 #endif
 
     for (int i = 0; i < size; ++i) {
-        void *next   = (*( void   **)tree) + point_offset + i * sizeof(void *);
+        void ***next = (*( void ** )tree) + point_offset;
         flags[depth] = i + 1 < size;
-        print_dynamic_tree_real(next, count_offset, point_offset, depth + 1, flags, print_tree_node);
+        print_dynamic_tree_real(&(*next)[i], count_offset, point_offset, depth + 1, flags, print_tree_node);
     }
 }
 
@@ -104,7 +104,7 @@ static void print_dynamic_tree_real(void *tree, int count_offset, int point_offs
  * 打印 N 叉树
  * @param tree             指向N叉树的指针
  * @param count_offset     孩子个数属性在结构体中的偏移量   offsetof(struct type, count)
- * @param point_offset     第一个孩子指针在结构体中的偏移量 offsetof(struct type, children[])
+ * @param point_offset     孩子二级指针在结构体中的偏移量   offsetof(struct type, children)
  * @param size             N 叉树孩子个数
  * @param depth            预测树的最大深度
  * @param print_tree_node  打印回调函数（函数中不可换行）
