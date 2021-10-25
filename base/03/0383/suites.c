18c5f897c2bca95ed8a0100ee5b354f90fe61721
Clarence <xjh.azzbcc@gmail.com>
2021-10-25 14:12:48 +0800

    0383 赎金信 解决

---
+++ base/03/0383/suites.c
@@ -0,0 +1,36 @@
+/**
+ * suites.c
+ * Copyright(c) 2021 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2021/10/25 下午2:08
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+
+#include "0383.c"
+
+START_TEST(test_official_1) {
+    char *ransomNote = "a", *magazine = "b";
+    bool target = false, ans = canConstruct(ransomNote, magazine);
+    ck_assert_int_eq(ans, target);
+}
+
+START_TEST(test_official_2) {
+    char *ransomNote = "aa", *magazine = "ab";
+    bool target = false, ans = canConstruct(ransomNote, magazine);
+    ck_assert_int_eq(ans, target);
+}
+
+START_TEST(test_official_3) {
+    char *ransomNote = "aa", *magazine = "aab";
+    bool target = true, ans = canConstruct(ransomNote, magazine);
+    ck_assert_int_eq(ans, target);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+    tcase_add_test(t, test_official_3);
+}
