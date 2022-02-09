dc590ef063df83f53e116f0185cb24a2aa9b036b
Clarence <xjh.azzbcc@gmail.com>
2022-02-09 11:53:08 +0800

    2006 差的绝对值为 K 的数对数目 解决

---
+++ base/20/2006/suites.c
@@ -0,0 +1,36 @@
+/**
+ * suites.c
+ * Copyright(c) 2022 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2022/2/9 上午11:42
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+
+#include "2006.c"
+
+START_TEST(test_official_1) {
+    int nums[] = { 1, 2, 2, 1 }, k = 1;
+    int target = 4, ans = countKDifference(nums, LEN(nums), k);
+    ck_assert_int_eq(ans, target);
+}
+
+START_TEST(test_official_2) {
+    int nums[] = { 1, 3 }, k = 3;
+    int target = 0, ans = countKDifference(nums, LEN(nums), k);
+    ck_assert_int_eq(ans, target);
+}
+
+START_TEST(test_official_3) {
+    int nums[] = { 3, 2, 1, 5, 4 }, k = 2;
+    int target = 3, ans = countKDifference(nums, LEN(nums), k);
+    ck_assert_int_eq(ans, target);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+    tcase_add_test(t, test_official_3);
+}
