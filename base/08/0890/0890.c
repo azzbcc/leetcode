5d4b0aa7d1418b272a254988e01f87aeed0fb606
Clarence <xjh.azzbcc@gmail.com>
2022-06-12 11:08:54 +0800

    0890 查找和替换模式 解决

---
+++ base/08/0890/0890.c
@@ -0,0 +1,54 @@
+// 你有一个单词列表 words 和一个模式 pattern，你想知道 words 中的哪些单词与模式匹配。
+//
+// 如果存在字母的排列 p ，使得将模式中的每个字母 x 替换为 p(x) 之后，我们就得到了所需的单词，那么单词与模式是匹配的。
+//
+// （回想一下，字母的排列是从字母到字母的双射：每个字母映射到另一个字母，没有两个字母映射到同一个字母。）
+//
+// 返回 words 中与给定模式匹配的单词列表。
+//
+// 你可以按任何顺序返回答案。
+//
+//
+//
+// 示例：
+//
+// 输入：words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb"
+// 输出：["mee","aqq"]
+// 解释：
+// "mee" 与模式匹配，因为存在排列 {a -> m, b -> e, ...}。
+// "ccc" 与模式不匹配，因为 {a -> c, b -> c, ...} 不是排列。
+// 因为 a 和 b 映射到同一个字母。
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= words.length <= 50
+// 1 <= pattern.length = words[i].length <= 20
+//
+// Related Topics 数组 哈希表 字符串 👍 148 👎 0
+
+#define WIDTH 26
+bool check(char *word, char *pattern) {
+    char map[WIDTH] = { '\0' };
+    for (int i = 0; word[i]; ++i) {
+        if (!map[word[i] - 'a']) {
+            map[word[i] - 'a'] = pattern[i];
+        } else if (map[word[i] - 'a'] != pattern[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+char **findAndReplacePattern(char **words, int size, char *pattern, int *returnSize) {
+    int len = 0;
+    char *help[size];
+    for (int i = 0; i < size; ++i) {
+        if (check(words[i], pattern) && check(pattern, words[i])) help[len++] = words[i];
+    }
+
+    char **ans = malloc((*returnSize = len) * sizeof(char *));
+    memcpy(ans, help, len * sizeof(char *));
+    return ans;
+}
