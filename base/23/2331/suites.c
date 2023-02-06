8aaa6ec815bd5a32610b9bea980cded3f245cd0d
Clarence <xjh.azzbcc@gmail.com>
2023-02-06 15:08:09 +0800

    2331 计算布尔二叉树的值 解决

---
+++ base/23/2331/suites.c
@@ -0,0 +1,32 @@
+/**
+ * suites.c
+ * Copyright(c) 2023 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2023/2/6 下午3:04
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+#include <tree.h>
+
+#include "2331.c"
+
+START_TEST(test_official_1) {
+    int arr[]   = { 2, 1, 3, null, null, 0, 1 };
+    tree_t root = tree_create(arr);
+    ck_assert_int_eq(true, evaluateTree(root));
+    tree_free(root);
+}
+
+START_TEST(test_official_2) {
+    int arr[]   = { 0 };
+    tree_t root = tree_create(arr);
+    ck_assert_int_eq(false, evaluateTree(root));
+    tree_free(root);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+}
