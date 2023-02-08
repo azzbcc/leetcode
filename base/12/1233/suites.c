80e289d63426abfbc5ffa30ce802e9b32ecf489e
Clarence <xjh.azzbcc@gmail.com>
2023-02-08 17:33:48 +0800

    1233 删除子文件夹 解决

---
+++ base/12/1233/suites.c
@@ -0,0 +1,54 @@
+/**
+ * suites.c
+ * Copyright(c) 2023 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2023/2/8 下午3:34
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+
+#include "1233.c"
+
+START_TEST(test_official_1) {
+    int len;
+    char *folder[] = { "/a", "/a/b", "/c/d", "/c/d/e", "/c/f" };
+    char *target[] = { "/a", "/c/d", "/c/f" };
+    char **ans     = removeSubfolders(folder, LEN(folder), &len);
+    ck_assert_int_eq(len, LEN(target));
+    for (int i = 0; i < len; ++i) {
+        ck_assert_str_eq(ans[i], target[i]);
+    }
+    free(ans);
+}
+
+START_TEST(test_official_2) {
+    int len;
+    char *folder[] = { "/a", "/a/b/c", "/a/b/d" };
+    char *target[] = { "/a" };
+    char **ans     = removeSubfolders(folder, LEN(folder), &len);
+    ck_assert_int_eq(len, LEN(target));
+    for (int i = 0; i < len; ++i) {
+        ck_assert_str_eq(ans[i], target[i]);
+    }
+    free(ans);
+}
+
+START_TEST(test_official_3) {
+    int len;
+    char *folder[] = { "/a/b/c", "/a/b/ca", "/a/b/d" };
+    char *target[] = { "/a/b/c", "/a/b/ca", "/a/b/d" };
+    char **ans     = removeSubfolders(folder, LEN(folder), &len);
+    ck_assert_int_eq(len, LEN(target));
+    for (int i = 0; i < len; ++i) {
+        ck_assert_str_eq(ans[i], target[i]);
+    }
+    free(ans);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+    tcase_add_test(t, test_official_3);
+}
