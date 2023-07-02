0643f2554055c53db1aacf7029a79a324246404a
Clarence <xjh.azzbcc@gmail.com>
2023-07-02 23:00:29 +0800

    0002 两数相加 优化

--- base/00/0002/0002.c
+++ base/00/0002/0002.c
@@ -1,5 +1,5 @@
-//给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储
-//一位 数字。
+// 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，
+// 并且它们的每个节点只能存储一位 数字。
 //
 // 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
 //
@@ -8,43 +8,21 @@
 // 示例：
 //
 // 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
-//输出：7 -> 0 -> 8
-//原因：342 + 465 = 807
+// 输出：7 -> 0 -> 8
+// 原因：342 + 465 = 807
 //
 // Related Topics 链表 数学
 // 👍 4646 👎 0
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-
 struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2) {
-    int val               = 0;
-    struct ListNode *head = NULL, *tail = NULL, *tmp = NULL;
-
-    while (l1 || l2 || val) {
+    struct ListNode *head = NULL, **tail = &head;
+    for (int val = 0; l1 || l2 || val; val /= 10) {
         if (l1) val += l1->val;
         if (l2) val += l2->val;
 
-        tmp       = calloc(1, sizeof(struct ListNode));
-        tmp->val  = val % 10;
-        tmp->next = NULL;
-
-        if (head == NULL) {
-            head = tmp;
-        } else {
-            tail->next = tmp;
-        }
-        tail = tmp;
-
+        *tail = calloc(1, sizeof(struct ListNode)), (*tail)->val = val % 10, tail = &(*tail)->next;
         if (l1) l1 = l1->next;
         if (l2) l2 = l2->next;
-        val /= 10;
     }
-
     return head;
 }
