0e37eddcbf9b9914a0f79cd56857a4044401c2ac
Clarence <xjh.azzbcc@gmail.com>
2023-07-10 15:33:12 +0800

    0016 最接近的三数之和 重做

--- base/00/0016/0016.c
+++ base/00/0016/0016.c
@@ -1,13 +1,24 @@
-// 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target
-// 最接近。返回这三个数的和 。假定每组输入只存在唯一答案。
+// 给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
+//
+// 返回这三个数的和。
+//
+// 假定每组输入只存在恰好一个解。
 //
 //
 //
-// 示例：
+// 示例 1：
+//
 //
 // 输入：nums = [-1,2,1,-4], target = 1
 // 输出：2
 // 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+//
+//
+// 示例 2：
+//
+//
+// 输入：nums = [0,0,0], target = 1
+// 输出：0
 //
 //
 //
@@ -15,44 +26,41 @@
 // 提示：
 //
 //
-// 3 <= nums.length <= 10^3
-// -10^3 <= nums[i] <= 10^3
-// -10^4 <= target <= 10^4
+// 3 <= nums.length <= 1000
+// -1000 <= nums[i] <= 1000
+// -10⁴ <= target <= 10⁴
 //
-// Related Topics 数组 双指针
-// 👍 530 👎 0
-int comp(const void *a, const void *b) {
+//
+// Related Topics 数组 双指针 排序 👍 1450 👎 0
+
+int cmp(const void *a, const void *b) {
     return *( int * )a - *( int * )b;
 }
 int next(int *nums, int size, int now) {
-    int cur = nums[now];
-    while (++now < size && nums[now] == cur) {}
+    for (int cur = nums[now]; ++now < size && nums[now] == cur;) {}
     return now;
 }
-int threeSumClosest(int *nums, int numsSize, int target) {
+int threeSumClosest(int *nums, int size, int target) {
     int ans = nums[0] + nums[1] + nums[2];
-    qsort(nums, numsSize, sizeof(nums[0]), comp);
-    for (int i = 0; i < numsSize - 2; i = next(nums, numsSize, i)) {
-        for (int j = i + 1, k = numsSize - 1; j < k; j = next(nums, k, j)) {
-            while (j < k && nums[i] + nums[j] + nums[k] < target) {
-                j = next(nums, k, j);
-            }
+    qsort(nums, size, sizeof(nums[0]), cmp);
+    for (int i = 0; i < size - 2; i = next(nums, size, i)) {
+        for (int j = i + 1, k = size - 1; j < k; j = next(nums, k, j)) {
+            for (; j < k && nums[i] + nums[j] + nums[k] < target; j = next(nums, k, j)) {}
             if (j <= k && j > i + 1) {
                 int cur = nums[i] + nums[j - 1] + nums[k];
-                if (abs(target - cur) < abs(target - ans)) { ans = cur; }
+                if (abs(target - cur) < abs(target - ans)) ans = cur;
             }
-            while (j < k && nums[i] + nums[j] + nums[k] > target) {
-                int cur = nums[k];
-                while (j < --k && nums[k] == cur) {}
+            for (; j < k && nums[i] + nums[j] + nums[k] > target;) {
+                for (int cur = nums[k]; j < --k && nums[k] == cur;) {}
             }
             if (j < k) {
                 int cur = nums[i] + nums[j] + nums[k];
-                if (abs(target - cur) < abs(target - ans)) { ans = cur; }
+                if (abs(target - cur) < abs(target - ans)) ans = cur;
                 if (ans == target) return ans;
             }
-            if (j <= k && k < numsSize - 1) {
+            if (j <= k && k < size - 1) {
                 int cur = nums[i] + nums[j] + nums[k + 1];
-                if (abs(target - cur) < abs(target - ans)) { ans = cur; }
+                if (abs(target - cur) < abs(target - ans)) ans = cur;
             }
         }
     }
--- base/00/0016/suites.c
+++ base/00/0016/suites.c
@@ -11,21 +11,29 @@
 
 #include "0016.c"
 
-START_TEST(test_official) {
+START_TEST(test_official_1) {
     int arr[] = { -1, 2, 1, -4 }, input = 1;
-    int target = 2, ans = threeSumClosest(arr, sizeof(arr) / sizeof(arr[0]), input);
+    int target = 2, ans = threeSumClosest(arr, LEN(arr), input);
+
+    ck_assert_int_eq(target, ans);
+}
+
+START_TEST(test_official_2) {
+    int arr[] = { 0, 0, 0 }, input = 1;
+    int target = 0, ans = threeSumClosest(arr, LEN(arr), input);
 
     ck_assert_int_eq(target, ans);
 }
 
 START_TEST(test_failed) {
     int arr[] = { -1, 0, 1, 1, 55 }, input = 3;
-    int target = 2, ans = threeSumClosest(arr, sizeof(arr) / sizeof(arr[0]), input);
+    int target = 2, ans = threeSumClosest(arr, LEN(arr), input);
 
     ck_assert_int_eq(target, ans);
 }
 
 void tcase_complete(TCase *t) {
     tcase_add_test(t, test_failed);
-    tcase_add_test(t, test_official);
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
 }
