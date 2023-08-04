c414d3a7fe5647761548676f6fe1ef8ad0ca5b8b
Clarence <xjh.azzbcc@gmail.com>
2023-08-05 00:30:07 +0800

    0021 合并两个有序链表 重做

--- base/00/0021/0021.c
+++ base/00/0021/0021.c
@@ -2,32 +2,47 @@
 //
 //
 //
-// 示例：
+// 示例 1：
 //
-// 输入：1->2->4, 1->3->4
-// 输出：1->1->2->3->4->4
 //
-// Related Topics 链表
-// 👍 1195 👎 0
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+// 输入：l1 = [1,2,4], l2 = [1,3,4]
+// 输出：[1,1,2,3,4,4]
+//
+//
+// 示例 2：
+//
+//
+// 输入：l1 = [], l2 = []
+// 输出：[]
+//
+//
+// 示例 3：
+//
+//
+// 输入：l1 = [], l2 = [0]
+// 输出：[0]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 两个链表的节点数目范围是 [0, 50]
+// -100 <= Node.val <= 100
+// l1 和 l2 均按 非递减顺序 排列
+//
+//
+// Related Topics 递归 链表 👍 3206 👎 0
 
 struct ListNode *mergeTwoLists(struct ListNode *l1, struct ListNode *l2) {
-    if (!l1) return l2;
-    if (!l2) return l1;
-
-    if (l1->val < l2->val) {
-        l1->next = mergeTwoLists(l1->next, l2);
-        return l1;
-    } else {
-        l2->next = mergeTwoLists(l1, l2->next);
-        return l2;
+    struct ListNode head = { .next = NULL }, *cur = &head;
+    for (; l1 || l2; cur = cur->next) {
+        if (l1 && l2 && l1->val < l2->val || !l2) {
+            cur->next = l1, l1 = l1->next;
+        } else {
+            cur->next = l2, l2 = l2->next;
+        }
     }
+    return head.next;
 }
-
--- base/00/0021/suites.c
+++ base/00/0021/suites.c
@@ -12,20 +12,44 @@
 
 #include "0021.c"
 
-START_TEST(test_official) {
+START_TEST(test_official_1) {
     int arr1[] = { 1, 2, 4 }, arr2[] = { 1, 3, 4 }, arr3[] = { 1, 1, 2, 3, 4, 4 };
-    list_t l1     = list_create(arr1);
-    list_t l2     = list_create(arr2);
-    list_t target = list_create(arr3);
+    list_t l1 = list_create(arr1), l2 = list_create(arr2), target = list_create(arr3);
 
     list_t ans = mergeTwoLists(l1, l2);
 
-    fail_if(!list_equal(target, ans));
+    ck_assert(list_equal(target, ans));
+
+    list_free(target);
+    list_free(ans);
+}
+
+START_TEST(test_official_2) {
+    int arr1[] = {}, arr2[] = {}, arr3[] = {};
+    list_t l1 = list_create(arr1), l2 = list_create(arr2), target = list_create(arr3);
+
+    list_t ans = mergeTwoLists(l1, l2);
+
+    ck_assert(list_equal(target, ans));
+
+    list_free(target);
+    list_free(ans);
+}
+
+START_TEST(test_official_3) {
+    int arr1[] = {}, arr2[] = { 0 }, arr3[] = { 0 };
+    list_t l1 = list_create(arr1), l2 = list_create(arr2), target = list_create(arr3);
+
+    list_t ans = mergeTwoLists(l1, l2);
+
+    ck_assert(list_equal(target, ans));
 
     list_free(target);
     list_free(ans);
 }
 
 void tcase_complete(TCase *t) {
-    tcase_add_test(t, test_official);
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+    tcase_add_test(t, test_official_3);
 }
