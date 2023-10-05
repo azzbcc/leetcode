092ba25d73952ef1e738321828644608c2ec1593
Clarence <xjh.azzbcc@gmail.com>
2023-10-05 23:40:46 +0800

    0069 x 的平方根 重做

--- base/00/0069/0069.c
+++ base/00/0069/0069.c
@@ -1,43 +1,55 @@
-// 实现 int sqrt(int x) 函数。
+// 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
 //
-// 计算并返回 x 的平方根，其中 x 是非负整数。
+// 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
 //
-// 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
-//
-// 示例 1:
-//
-// 输入: 4
-// 输出: 2
+// 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
 //
 //
-// 示例 2:
 //
-// 输入: 8
-// 输出: 2
-// 说明: 8 的平方根是 2.82842...,
-//    由于返回类型是整数，小数部分将被舍去。
+// 示例 1：
 //
-// Related Topics 数学 二分查找
-// 👍 489 👎 0
+//
+// 输入：x = 4
+// 输出：2
+//
+//
+// 示例 2：
+//
+//
+// 输入：x = 8
+// 输出：2
+// 解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= x <= 2³¹ - 1
+//
+//
+// Related Topics 数学 二分查找 👍 1448 👎 0
 
-int mySqrt_1(int x) {
-    int m = x, n = 1;
-    while (m > n) {
-        m = n + (m - n) / 2, n = x / m;
-    }
-    return m;
+#if 0
+int mySqrt(int x) {
+    for (int a = x, b = 1; x > b; x = b + (x - b) / 2, b = a / x) {}
+    return x;
 }
+#else
+typedef struct {
+    int a, b;
+} data_t;
+data_t my_sqrt(int x) {
+    data_t ans = { 0 };
+    if (x >= 100) ans = my_sqrt(x / 100);
 
-int my_sqrt(int x, int *mod) {
-    int tmp = 0, n = 1;
-    if (x >= 100) tmp = my_sqrt(x / 100, mod);
-    for (*mod = *mod * 100 + x % 100; n < 10 && n * (20 * tmp + n) <= *mod; n++) {}
-    n -= 1, *mod = *mod - n * (20 * tmp + n);
-    return tmp * 10 + n;
+    int n = 1;
+    for (ans.b = ans.b * 100 + x % 100; n < 10 && n * (20 * ans.a + n) <= ans.b; ++n) {}
+    n--, ans.b = ans.b - n * (20 * ans.a + n), ans.a = ans.a * 10 + n;
+    return ans;
 }
-int mySqrt_2(int x) {
-    int mod = 0;
-    return my_sqrt(x, &mod);
+int mySqrt(int x) {
+    return my_sqrt(x).a;
 }
-
-int (*mySqrt)(int x) = mySqrt_2;
+#endif
