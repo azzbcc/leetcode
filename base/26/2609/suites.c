5186276f1dad434a5f14baf1ad23f62d96697678
Clarence <xjh.azzbcc@gmail.com>
2023-11-08 15:22:35 +0800

    2609 最长平衡子字符串 解决

---
+++ base/26/2609/suites.c
@@ -0,0 +1,43 @@
+/**
+ * suites.c
+ * Copyright(c) 2023 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2023/11/8 15:08
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+
+#include "2609.c"
+
+START_TEST(test_official_1) {
+    char *s    = "01000111";
+    int target = 6, ans = findTheLongestBalancedSubstring(s);
+    ck_assert_int_eq(ans, target);
+}
+
+START_TEST(test_official_2) {
+    char *s    = "00111";
+    int target = 4, ans = findTheLongestBalancedSubstring(s);
+    ck_assert_int_eq(ans, target);
+}
+
+START_TEST(test_official_3) {
+    char *s    = "111";
+    int target = 0, ans = findTheLongestBalancedSubstring(s);
+    ck_assert_int_eq(ans, target);
+}
+
+START_TEST(test_own) {
+    char *s    = "11100";
+    int target = 0, ans = findTheLongestBalancedSubstring(s);
+    ck_assert_int_eq(ans, target);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_own);
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+    tcase_add_test(t, test_official_3);
+}
