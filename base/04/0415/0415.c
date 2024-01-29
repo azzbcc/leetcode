d7e10797af53f201df3bfbdebf8f45e522e26d30
Clarence <xjh.azzbcc@gmail.com>
2024-01-29 16:29:49 +0800

    0415 字符串相加 重做

--- base/04/0415/0415.c
+++ base/04/0415/0415.c
@@ -1,31 +1,53 @@
-//给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+// 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和并同样以字符串形式返回。
 //
-// 注意：
+// 你不能使用任何內建的用于处理大整数的库（比如 BigInteger）， 也不能直接将输入的字符串转换为整数形式。
 //
 //
-// num1 和num2 的长度都小于 5100.
-// num1 和num2 都只包含数字 0-9.
-// num1 和num2 都不包含任何前导零。
-// 你不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式。
 //
-// Related Topics 字符串
-// 👍 211 👎 0
+// 示例 1：
+//
+//
+// 输入：num1 = "11", num2 = "123"
+// 输出："134"
+//
+//
+// 示例 2：
+//
+//
+// 输入：num1 = "456", num2 = "77"
+// 输出："533"
+//
+//
+// 示例 3：
+//
+//
+// 输入：num1 = "0", num2 = "0"
+// 输出："0"
+//
+//
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= num1.length, num2.length <= 10⁴
+// num1 和num2 都只包含数字 0-9
+// num1 和num2 都不包含任何前导零
+//
+//
+// Related Topics 数学 字符串 模拟 👍 811 👎 0
 
-#define MAX 5100
-char *addStrings(char *num1, char *num2) {
-    static char ans[MAX + 1] = { 0 };
-    int carry = 0, l1 = strlen(num1), l2 = strlen(num2), la = MAX;
+#define MAX 10000
+char *addStrings(char *n1, char *n2) {
+    static char str[MAX + 2] = { '\0' };
 
-    while (l1 || l2 || carry) {
-        if (l1) carry += num1[l1 - 1] - '0';
-        if (l2) carry += num2[l2 - 1] - '0';
-
-        ans[--la] = '0' + carry % 10;
-
-        if (l1) l1 -= 1;
-        if (l2) l2 -= 1;
-        carry /= 10;
+    int pos = MAX + 1;
+    for (int c = 0, l1 = strlen(n1), l2 = strlen(n2); c || l1 || l2; c /= 10) {
+        if (l1) c += n1[--l1] - '0';
+        if (l2) c += n2[--l2] - '0';
+        str[--pos] = c % 10 + '0';
     }
-
-    return &ans[la];
+    return &str[pos];
 }
--- base/04/0415/suites.c
+++ base/04/0415/suites.c
@@ -15,10 +15,30 @@
 START_TEST(test_own) {
     char *num1 = "987654321", *num2 = "12345689";
     char *target = "1000000010", *ans = addStrings(num1, num2);
+    ck_assert_str_eq(ans, target);
+}
 
-    ck_assert_msg(!strcmp(target, ans), "error, except %s but got %s on addStrings().", target, ans);
+START_TEST(test_official_1) {
+    char *num1 = "11", *num2 = "123";
+    char *target = "134", *ans = addStrings(num1, num2);
+    ck_assert_str_eq(ans, target);
+}
+
+START_TEST(test_official_2) {
+    char *num1 = "456", *num2 = "77";
+    char *target = "533", *ans = addStrings(num1, num2);
+    ck_assert_str_eq(ans, target);
+}
+
+START_TEST(test_official_3) {
+    char *num1 = "0", *num2 = "0";
+    char *target = "0", *ans = addStrings(num1, num2);
+    ck_assert_str_eq(ans, target);
 }
 
 void tcase_complete(TCase *t) {
     tcase_add_test(t, test_own);
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+    tcase_add_test(t, test_official_3);
 }
