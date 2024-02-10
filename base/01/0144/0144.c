f278bfecdae73bc4fff2c71b293e7510d5a07ff2
Clarence <xjh.azzbcc@gmail.com>
2024-02-11 00:33:23 +0800

    0144 二叉树的前序遍历 重做

--- base/01/0144/0144.c
+++ base/01/0144/0144.c
@@ -1,36 +1,68 @@
-// 给定一个二叉树，返回它的 前序 遍历。
-//
-// 示例:
-//
-// 输入: [1,null,2,3]
-//   1
-//    \
-//     2
-//    /
-//   3
-//
-// 输出: [1,2,3]
+// 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
 //
 //
-// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
-// Related Topics 栈 树
-// 👍 413 👎 0
+//
+// 示例 1：
+//
+//
+// 输入：root = [1,null,2,3]
+// 输出：[1,2,3]
+//
+//
+// 示例 2：
+//
+//
+// 输入：root = []
+// 输出：[]
+//
+//
+// 示例 3：
+//
+//
+// 输入：root = [1]
+// 输出：[1]
+//
+//
+// 示例 4：
+//
+//
+// 输入：root = [1,2]
+// 输出：[1,2]
+//
+//
+// 示例 5：
+//
+//
+// 输入：root = [1,null,2]
+// 输出：[1,2]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1213 👎 0
 
-#define MAXN 0x1000
+#define MAX 100
 int *preorderTraversal(struct TreeNode *root, int *returnSize) {
-    int help[MAXN], len = 0, *ans = NULL;
+    int help[MAX], len = 0;
 
-    while (root) {
+    for (struct TreeNode *morris; root;) {
         if (root->left) {
-            struct TreeNode *morris_prev = root->left;
-            while (morris_prev->right && morris_prev->right != root) {
-                morris_prev = morris_prev->right;
-            }
-            if (morris_prev->right) {
-                morris_prev->right = NULL;
+            for (morris = root->left; morris->right && morris->right != root; morris = morris->right) {}
+            if (morris->right) {
+                morris->right = NULL;
             } else {
-                help[len++]        = root->val;
-                morris_prev->right = root, root = root->left;
+                help[len++] = root->val, morris->right = root, root = root->left;
                 continue;
             }
         } else {
@@ -39,8 +71,7 @@ int *preorderTraversal(struct TreeNode *root, int *returnSize) {
         root = root->right;
     }
 
-    *returnSize = len, ans = calloc(len, sizeof(int));
+    int *ans = malloc((*returnSize = len) * sizeof(int));
     memcpy(ans, help, len * sizeof(int));
-
     return ans;
 }
--- base/01/0144/suites.c
+++ base/01/0144/suites.c
@@ -12,35 +12,71 @@
 
 #include "0144.c"
 
-START_TEST(test_official) {
+START_TEST(test_official_1) {
     int arr[] = { 1, null, 2, 3 }, target[] = { 1, 2, 3 };
-
     tree_t t = tree_create(arr);
-
     int len, *ans = preorderTraversal(t, &len);
-
     ck_assert_int_eq(len, LEN(target));
     ck_assert_mem_eq(ans, target, sizeof(target));
+    tree_free(t);
+    free(ans);
+}
 
+START_TEST(test_official_2) {
+    int arr[] = {}, target[] = {};
+    tree_t t = tree_create(arr);
+    int len, *ans = preorderTraversal(t, &len);
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
+    tree_free(t);
+    free(ans);
+}
+
+START_TEST(test_official_3) {
+    int arr[] = { 1 }, target[] = { 1 };
+    tree_t t = tree_create(arr);
+    int len, *ans = preorderTraversal(t, &len);
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
+    tree_free(t);
+    free(ans);
+}
+
+START_TEST(test_official_4) {
+    int arr[] = { 1, 2 }, target[] = { 1, 2 };
+    tree_t t = tree_create(arr);
+    int len, *ans = preorderTraversal(t, &len);
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
+    tree_free(t);
+    free(ans);
+}
+
+START_TEST(test_official_5) {
+    int arr[] = { 1, null, 2 }, target[] = { 1, 2 };
+    tree_t t = tree_create(arr);
+    int len, *ans = preorderTraversal(t, &len);
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
     tree_free(t);
     free(ans);
 }
 
 START_TEST(test_own) {
     int arr[] = { 1, 2, 3, 4, 5, 6, 7 }, target[] = { 1, 2, 4, 5, 3, 6, 7 };
-
     tree_t t = tree_create(arr);
-
     int len, *ans = preorderTraversal(t, &len);
-
     ck_assert_int_eq(len, LEN(target));
     ck_assert_mem_eq(ans, target, sizeof(target));
-
     tree_free(t);
     free(ans);
 }
 
 void tcase_complete(TCase *t) {
     tcase_add_test(t, test_own);
-    tcase_add_test(t, test_official);
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+    tcase_add_test(t, test_official_3);
+    tcase_add_test(t, test_official_4);
+    tcase_add_test(t, test_official_5);
 }
