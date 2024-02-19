197db3d10d239a7f5e0c468f5daf1a1f1bb0ec83
Clarence <xjh.azzbcc@gmail.com>
2024-02-19 16:21:10 +0800

    0590 N 叉树的后序遍历 解决

---
+++ base/05/0590/suites.c
@@ -0,0 +1,85 @@
+/**
+ * suites.c
+ * Copyright(c) 2024 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2024/2/19 14:46
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+
+typedef struct Node {
+    int val;
+    int numChildren;
+    struct Node **children;
+} *node_t;
+
+#include "0590.c"
+
+#define null INT32_MIN
+
+static node_t node_create(int *arr, size_t size) {
+    if (!size) return NULL;
+    node_t nodes[size];
+
+    for (int i = 0; i < size; ++i) {
+        nodes[i] = arr[i] == null ? NULL : calloc(1, sizeof(struct Node));
+        if (nodes[i]) nodes[i]->val = arr[i];
+    }
+    if (nodes[1]) free(nodes[1]);
+    for (int l = 2, r, p = 0; l < size; l = r + 1) {
+        for (r = l; r < size && nodes[r]; ++r) {}
+        nodes[p]->children = malloc((nodes[p]->numChildren = r - l) * sizeof(node_t));
+        memcpy(nodes[p]->children, nodes + l, nodes[p]->numChildren * sizeof(node_t));
+        for (++p; p < size && !nodes[p]; ++p) {}
+    }
+    return nodes[0];
+}
+static void node_free(node_t tree) {
+    if (!tree) return;
+    for (int i = 0; i < tree->numChildren; ++i) {
+        node_free(tree->children[i]);
+    }
+    if (tree->children) free(tree->children);
+    free(tree);
+}
+
+START_TEST(test_official_1) {
+    int arr[] = { 1, null, 3, 2, 4, null, 5, 6 }, target[] = { 5, 6, 3, 2, 4, 1 };
+    node_t tree = node_create(arr, LEN(arr));
+    int len, *ans = postorder(tree, &len);
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
+    free(ans);
+    node_free(tree);
+}
+
+START_TEST(test_official_2) {
+    int arr[]    = { 1, null, 2,    3,    4,  5,    null, null, 6,  7,    null, 8, null,
+                     9, 10,   null, null, 11, null, 12,   null, 13, null, null, 14 };
+    int target[] = { 2, 6, 14, 11, 7, 3, 12, 8, 4, 13, 9, 10, 5, 1 };
+    node_t tree  = node_create(arr, LEN(arr));
+    int len, *ans = postorder(tree, &len);
+    ck_assert_int_eq(len, LEN(target));
+    ck_assert_mem_eq(ans, target, sizeof(target));
+    free(ans);
+    node_free(tree);
+}
+
+START_TEST(test_own) {
+    int max_len = 10000, arr[max_len + 1];
+    memset(arr, 0, sizeof(arr));
+    node_t tree = node_create(arr, LEN(arr));
+    int len, *ans = postorder(tree, &len);
+    ck_assert_int_eq(len, max_len);
+    ck_assert_mem_eq(ans, arr, len * sizeof(int));
+    free(ans);
+    node_free(tree);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_own);
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+}
