8a74c441cccc8020fd78332710c43da98a395ae5
Clarence <xjh.azzbcc@gmail.com>
2024-02-25 20:41:31 +0800

    0235 二叉搜索树的最近公共祖先 重做

--- base/02/0235/0235.c
+++ base/02/0235/0235.c
@@ -1,8 +1,7 @@
-//给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+// 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
 //
-// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且
-// x 的深度尽可能大（
-//一个节点也可以是它自己的祖先）。”
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，
+// 最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
 //
 // 例如，给定如下二叉搜索树: root = [6,2,8,0,4,7,9,null,null,3,5]
 //
@@ -13,15 +12,15 @@
 // 示例 1:
 //
 // 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
-//输出: 6
-//解释: 节点 2 和节点 8 的最近公共祖先是 6。
+// 输出: 6
+// 解释: 节点 2 和节点 8 的最近公共祖先是 6。
 //
 //
 // 示例 2:
 //
 // 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
-//输出: 2
-//解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+// 输出: 2
+// 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
 //
 //
 //
@@ -31,17 +30,19 @@
 // 所有节点的值都是唯一的。
 // p、q 为不同节点且均存在于给定的二叉搜索树中。
 //
-// Related Topics 树
-// 👍 426 👎 0
-
-struct TreeNode *dfs(struct TreeNode *root, int a, int b) {
-    if (a <= root->val && b >= root->val) return root;
-    return dfs(a > root->val ? root->right : root->left, a, b);
-}
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 1219 👎 0
 
 struct TreeNode *lowestCommonAncestor(struct TreeNode *root, struct TreeNode *p, struct TreeNode *q) {
-    int a = p->val, b = q->val;
-    for (int tmp = a; a > b; a = b, b = tmp) {}
-
-    return dfs(root, a, b);
+    int max = fmax(p->val, q->val), min = fmin(p->val, q->val);
+    for (; root;) {
+        if (root->val < min) {
+            root = root->right;
+        } else if (root->val > max) {
+            root = root->left;
+        } else {
+            break;
+        }
+    }
+    return root;
 }
