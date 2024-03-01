bbf7af3bdca7ade0f65ed2c297c7c11a2b0316d6
Clarence <xjh.azzbcc@gmail.com>
2024-03-01 13:55:31 +0800

    2369 检查数组是否存在有效划分 解决

---
+++ base/23/2369/suites.c
@@ -0,0 +1,29 @@
+/**
+ * suites.c
+ * Copyright(c) 2024 beijing. All rights reserved.
+ * Description:
+ * Version    :
+ * Created on : 2024/3/1 13:27
+ * Modified   :
+ *     Author : Clarence <xjh.azzbcc@gmail.com>
+ */
+#include <common.h>
+
+#include "2369.c"
+
+START_TEST(test_official_1) {
+    int nums[]  = { 4, 4, 4, 5, 6 };
+    bool target = true, ans = validPartition(nums, LEN(nums));
+    ck_assert_int_eq(ans, target);
+}
+
+START_TEST(test_official_2) {
+    int nums[]  = { 1, 1, 1, 2 };
+    bool target = false, ans = validPartition(nums, LEN(nums));
+    ck_assert_int_eq(ans, target);
+}
+
+void tcase_complete(TCase *t) {
+    tcase_add_test(t, test_official_1);
+    tcase_add_test(t, test_official_2);
+}
