ce057f986aba72e7de10e96829c01aa0ef7697b8
Clarence <xjh.azzbcc@gmail.com>
2024-03-04 15:37:43 +0800

    0232 用栈实现队列 重做

--- base/02/0232/0232.c
+++ base/02/0232/0232.c
@@ -1,4 +1,4 @@
-// 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列的支持的所有操作（push、pop、peek、empty）：
+// 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
 //
 // 实现 MyQueue 类：
 //
@@ -9,28 +9,16 @@
 // boolean empty() 如果队列为空，返回 true ；否则，返回 false
 //
 //
-//
-//
 // 说明：
 //
 //
-// 你只能使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
-//
+// 你 只能 使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
 // 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
 //
 //
 //
 //
-// 进阶：
-//
-//
-// 你能否实现每个操作均摊时间复杂度为 O(1) 的队列？换句话说，执行 n 个操作的总时间复杂度为 O(n)
-// ，即使其中一个操作可能花费较长时间。
-//
-//
-//
-//
-// 示例：
+// 示例 1：
 //
 //
 // 输入：
@@ -60,66 +48,81 @@
 // 最多调用 100 次 push、pop、peek 和 empty
 // 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）
 //
-// Related Topics 栈 设计
-// 👍 288 👎 0
+//
+//
+//
+// 进阶：
+//
+//
+// 你能否实现每个操作均摊时间复杂度为 O(1) 的队列？换句话说，执行 n 个操作的总时间复杂度为 O(n)
+// ，即使其中一个操作可能花费较长时间。
+//
+//
+// Related Topics 栈 设计 队列 👍 1068 👎 0
 
-#define MAXN 100
+typedef struct stack_node *stack_t;
+struct stack_node {
+    int val;
+    stack_t next;
+};
+static stack_t stack_new(int val, stack_t next) {
+    stack_t s = malloc(sizeof(*s));
+    s->val = val, s->next = next;
+    return s;
+}
+bool stack_empty(stack_t s) {
+    return s == NULL;
+}
+void stack_push(stack_t *s, int val) {
+    *s = stack_new(val, *s);
+}
+int stack_peak(stack_t s) {
+    return s ? s->val : -1;
+}
+int stack_pop(stack_t *s) {
+    int val = stack_peak(*s);
+    if (stack_empty(*s)) return val;
 
+    stack_t next = (*s)->next;
+    free(*s);
+    *s = next;
+    return val;
+}
+void stack_destroy(stack_t *s) {
+    for (stack_t now = *s, next; now; now = next) {
+        next = now->next;
+        free(now);
+    }
+    *s = NULL;
+}
 typedef struct {
-    int top;
-    int data[MAXN];
-} sstack_t;
-void stack_init(sstack_t *s) {
-    s->top = -1;
-}
-void stack_push(sstack_t *s, int val) {
-    s->data[++s->top] = val;
-}
-int stack_pop(sstack_t *s) {
-    return s->data[s->top--];
-}
-int stack_peek(sstack_t *s) {
-    return s->data[s->top];
-}
-bool stack_empty(sstack_t *s) {
-    return s->top == -1;
-}
-
-typedef struct {
-    sstack_t in[1], out[1];
+    stack_t in, out;
 } MyQueue;
-
-MyQueue *myQueueCreate() {
-    MyQueue *queue = malloc(sizeof(MyQueue));
-    stack_init(queue->in), stack_init(queue->out);
-    return queue;
-}
-
-void myQueuePush(MyQueue *queue, int x) {
-    stack_push(queue->in, x);
-}
-
-static void queue_migrate(MyQueue *queue) {
-    if (!stack_empty(queue->out)) return;
-    while (!stack_empty(queue->in)) {
-        stack_push(queue->out, stack_pop(queue->in));
+static void queue_migrate(MyQueue *q) {
+    if (!stack_empty(q->out)) return;
+    for (; !stack_empty(q->in);) {
+        stack_push(&q->out, stack_pop(&q->in));
     }
 }
-
-int myQueuePop(MyQueue *queue) {
-    queue_migrate(queue);
-    return stack_pop(queue->out);
+MyQueue *myQueueCreate() {
+    return calloc(1, sizeof(MyQueue));
 }
-
-int myQueuePeek(MyQueue *queue) {
-    queue_migrate(queue);
-    return stack_peek(queue->out);
+void myQueuePush(MyQueue *q, int x) {
+    stack_push(&q->in, x);
 }
-
-bool myQueueEmpty(MyQueue *queue) {
-    return stack_empty(queue->in) && stack_empty(queue->out);
+int myQueuePop(MyQueue *q) {
+    queue_migrate(q);
+    return stack_pop(&q->out);
 }
-
-void myQueueFree(MyQueue *queue) {
-    free(queue);
+int myQueuePeek(MyQueue *q) {
+    queue_migrate(q);
+    return stack_peak(q->out);
+}
+bool myQueueEmpty(MyQueue *q) {
+    return stack_empty(q->in) && stack_empty(q->out);
+}
+void myQueueFree(MyQueue *q) {
+    stack_destroy(&q->out);
+    stack_destroy(&q->in);
+    free(q);
 }
