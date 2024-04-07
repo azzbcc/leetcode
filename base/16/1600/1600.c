fdb18f94c5b952aacdaf32e094fc80b60e29bc5d
Clarence <xjh.azzbcc@gmail.com>
2024-04-07 20:59:47 +0800

    1600 王位继承顺序 重做

--- base/16/1600/1600.c
+++ base/16/1600/1600.c
@@ -1,7 +1,8 @@
 // 一个王国里住着国王、他的孩子们、他的孙子们等等。每一个时间点，这个家庭里有人出生也有人死亡。
 //
-// 这个王国有一个明确规定的皇位继承顺序，第一继承人总是国王自己。我们定义递归函数 Successor(x, curOrder) ，给定一个人 x
-// 和当前的继承顺序，该函数返回 x 的下一继承人。
+// 这个王国有一个明确规定的王位继承顺序，第一继承人总是国王自己。
+// 我们定义递归函数 Successor(x, curOrder) ，给定一个人 x 和当前的继承顺序，该函数返回 x 的下一继承人。
+//
 //
 // Successor(x, curOrder):
 //    如果 x 没有孩子或者所有 x 的孩子都在 curOrder 中：
@@ -14,8 +15,8 @@
 //
 //
 // 一开始， curOrder 为 ["king"].
-// 调用 Successor(king, curOrder) ，返回 Alice ，所以我们将 Alice 放入 curOrder 中，得到 ["king","Alice"] 。
-// 调用 Successor(Alice, curOrder) ，返回 Jack ，所以我们将 Jack 放入 curOrder 中，得到 ["king","Alice", "Jack"] 。
+// 调用 Successor(king, curOrder) ，返回 Alice ，所以我们将 Alice 放入 curOrder 中，得到 ["king", "Alice"] 。
+// 调用 Successor(Alice, curOrder) ，返回 Jack ，所以我们将 Jack 放入 curOrder 中，得到 ["king", "Alice", "Jack"] 。
 // 调用 Successor(Jack, curOrder) ，返回 Bob ，所以我们将 Bob 放入 curOrder 中，得到 ["king", "Alice", "Jack", "Bob"] 。
 // 调用 Successor(Bob, curOrder) ，返回 null 。最终得到继承顺序为 ["king", "Alice", "Jack", "Bob"] 。
 //
@@ -27,24 +28,27 @@
 //
 // ThroneInheritance(string kingName) 初始化一个 ThroneInheritance 类的对象。国王的名字作为构造函数的参数传入。
 // void birth(string parentName, string childName) 表示 parentName 新拥有了一个名为 childName 的孩子。
-// void death(string name) 表示名为 name 的人死亡。一个人的死亡不会影响 Successor
-// 函数，也不会影响当前的继承顺序。你可以只将这个人标记为死亡状态。 string[] getInheritanceOrder() 返回 除去
-// 死亡人员的当前继承顺序列表。
+// void death(string name) 表示名为 name 的人死亡。一个人的死亡不会影响 Successor 函数，也不会影响当前的继承顺序。
+// 你可以只将这个人标记为死亡状态。
+// string[] getInheritanceOrder() 返回 除去 死亡人员的当前继承顺序列表。
 //
 //
 //
 //
 // 示例：
 //
-// 输入：
-// ["ThroneInheritance", "birth", "birth", "birth", "birth", "birth", "birth", "getInheritanceOrder", "death",
-// "getInheritanceOrder"]
-// [["king"], ["king", "andy"], ["king", "bob"], ["king", "catherine"], ["andy","matthew"], ["bob", "alex"], ["bob",
-// "asha"], [null], ["bob"], [null]]
 //
+// 输入：
+// ["ThroneInheritance", "birth", "birth", "birth", "birth", "birth",
+// "birth", "getInheritanceOrder", "death",
+// "getInheritanceOrder"]
+// [["king"], ["king", "andy"], ["king", "bob"], ["king", "catherine"], ["andy", "matthew"], ["bob", "alex"],
+// ["bob", "asha"], [null], ["bob"],
+// [null]]
 // 输出：
-// [null, null, null, null, null, null, null, ["king", "andy", "matthew", "bob",
-// "alex", "asha", "catherine"], null, ["king", "andy", "matthew", "alex", "asha","catherine"]]
+// [null, null, null, null, null, null,
+// null, ["king", "andy", "matthew", "bob", "alex", "asha", "catherine"], null,
+// ["king", "andy", "matthew", "alex", "asha", "catherine"]]
 //
 // 解释：
 // ThroneInheritance t= new ThroneInheritance("king"); // 继承顺序：king
@@ -53,7 +57,6 @@
 // t.birth("king", "catherine"); // 继承顺序：king > andy > bob > catherine
 // t.birth("andy", "matthew"); // 继承顺序：king > andy > matthew > bob > catherine
 // t.birth("bob", "alex"); // 继承顺序：king > andy > matthew > bob > alex > catherine
-//
 // t.birth("bob", "asha"); // 继承顺序：king > andy > matthew > bob > alex > asha > catherine
 // t.getInheritanceOrder(); // 返回 ["king", "andy", "matthew", "bob", "alex", "asha", "catherine"]
 // t.death("bob"); // 继承顺序：king > andy > matthew > bob（已经去世）> alex > asha > catherine
@@ -71,32 +74,30 @@
 // 所有的参数 childName 和 kingName 互不相同。
 // 所有 death 函数中的死亡名字 name 要么是国王，要么是已经出生了的人员名字。
 // 每次调用 birth(parentName, childName) 时，测试用例都保证 parentName 对应的人员是活着的。
-// 最多调用 105 次birth 和 death 。
+// 最多调用 10⁵ 次birth 和 death 。
 // 最多调用 10 次 getInheritanceOrder 。
 //
-// Related Topics 树 设计
-// 👍 45 👎 0
+//
+// Related Topics 树 深度优先搜索 设计 哈希表 👍 98 👎 0
 
 typedef struct node {
     bool death;
-    char *name;
+    char name[16];
     struct node *child, *last, *brother;
     UT_hash_handle hh;
 } node_t, *hash_t, *tree_t;
 typedef struct {
     int count;
-    tree_t tree;
-    hash_t hash;
+    hash_t hash, root;
 } ThroneInheritance;
 static node_t *node_new(const char *name) {
-    node_t *data = malloc(sizeof(*data));
-    data->death = false, data->name = strdup(name);
-    data->child = data->last = data->brother = NULL;
-    return data;
+    node_t *node = calloc(1, sizeof(*node));
+    strcpy(node->name, name);
+    return node;
 }
-static hash_t hash_find(ThroneInheritance *inheritance, char *name) {
-    hash_t root = inheritance ? inheritance->hash : NULL, find = NULL;
-    HASH_FIND_STR(root, name, find);
+static hash_t hash_find(hash_t t, char *name) {
+    hash_t find = NULL;
+    HASH_FIND_STR(t, name, find);
     return find;
 }
 static void dfs(tree_t root, char ***ans) {
@@ -106,14 +107,14 @@ static void dfs(tree_t root, char ***ans) {
     dfs(root->brother, ans);
 }
 ThroneInheritance *throneInheritanceCreate(char *king) {
-    node_t *root                   = node_new(king);
     ThroneInheritance *inheritance = malloc(sizeof(ThroneInheritance));
-    inheritance->hash = NULL, inheritance->tree = root, inheritance->count = 1;
-    HASH_ADD_STR(inheritance->hash, name, root);
+    inheritance->root              = node_new(king);
+    inheritance->hash = NULL, inheritance->count = 1;
+    HASH_ADD_STR(inheritance->hash, name, inheritance->root);
     return inheritance;
 }
 void throneInheritanceBirth(ThroneInheritance *inheritance, char *parentName, char *childName) {
-    hash_t parent = hash_find(inheritance, parentName);
+    hash_t parent = hash_find(inheritance->hash, parentName);
     if (!parent) return;
 
     node_t *child = node_new(childName);
@@ -125,13 +126,13 @@ void throneInheritanceBirth(ThroneInheritance *inheritance, char *parentName, ch
     HASH_ADD_STR(inheritance->hash, name, child);
 }
 void throneInheritanceDeath(ThroneInheritance *inheritance, char *name) {
-    hash_t death = hash_find(inheritance, name);
+    hash_t death = hash_find(inheritance->hash, name);
     if (!death) return;
     inheritance->count -= 1, death->death = true;
 }
 char **throneInheritanceGetInheritanceOrder(ThroneInheritance *inheritance, int *retSize) {
     char **ans = malloc((*retSize = inheritance->count) * sizeof(char *)), **tmp = ans;
-    dfs(inheritance->tree, &tmp);
+    dfs(inheritance->root, &tmp);
     return ans;
 }
 void throneInheritanceFree(ThroneInheritance *inheritance) {
@@ -139,7 +140,6 @@ void throneInheritanceFree(ThroneInheritance *inheritance) {
     hash_t root = inheritance->hash, cur, next;
     HASH_ITER(hh, root, cur, next) {
         HASH_DEL(root, cur);
-        free(cur->name);
         free(cur);
     }
     free(inheritance);
